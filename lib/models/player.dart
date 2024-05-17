95cb9e2a379b7102b3e519fc9f2bc5dbd8fe21e9
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2024-05-17 19:03:03 +0700

    first commit

---
+++ lib/models/player.dart
@@ -0,0 +1,12 @@
+class Player {
+  String? id, name, position, imageUrl;
+  DateTime? createdAt;
+
+  Player({
+    this.position,
+    this.id,
+    this.imageUrl,
+    this.name,
+    this.createdAt,
+  });
+}
